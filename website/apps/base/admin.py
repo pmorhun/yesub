6524d4a2540db0f5be1f469f482576d5221f2e3a
pmorhun <petermorhun@gmail.com>
2017-08-19 15:42:41 +0300

    add model podcast

--- website/apps/base/admin.py
+++ website/apps/base/admin.py
@@ -2,5 +2,15 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from base.models import Podcast
 
 # Register your models here.
+
+
+class PodcastAdmin(admin.ModelAdmin):
+    model = Podcast
+    fields = ('title', 'text', 'is_displayed', 'priority', 'slug')
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Podcast, PodcastAdmin)
--- website/apps/base/models.py
+++ website/apps/base/models.py
@@ -4,3 +4,29 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class DisplayManager(models.Manager):
+    def get_queryset(self):
+        return super(DisplayManager, self).get_queryset().filter(is_displayed=True)
+
+
+class DisplayModel(models.Model):
+    is_displayed = models.BooleanField(default=True)
+    priority = models.IntegerField(default=0)
+
+    objects = models.Manager()  # The default manager.
+    displayed_objects = DisplayManager()
+
+    class Meta:
+        abstract = True
+        ordering = ('priority',)
+
+
+class Podcast(DisplayModel):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(unique=True, null=True, blank=True)
+    text = models.TextField(null=True, blank=True)
+
+    def __unicode__(self):
+        return self.title
